c65eb60e8f91466b9282d90eed164bb2756b3a15
jimmypunk <wang.jimmy319@gmail.com>
2015-11-24 11:13:57 -0800

    Remove code related to server-side and the original main.py

--- nameit_site/smart_naming/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^word_profile/(?P<word>[a-zA-Z][_a-zA-Z0-9]+)', views.word_profile, name='word_profile'),
-]
